fee947cf12b36d22e56828a5743c8256edebcc6a
SenpaiKush <32723898+SenpaiKush@users.noreply.github.com>
2017-11-19 19:30:21 -0500

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
@@ -7,10 +7,13 @@ public class Player : MonoBehaviour {
 
 	public float speed;
 	public float offset;
+	public Rigidbody rb;
+	public GameObject fridge;
+
 
 	public void SetLocation (MazeCell cell) {
-		//currentCell = cell;
-		//transform.localPosition = cell.transform.localPosition;
+		currentCell = cell;
+		transform.localPosition = cell.transform.localPosition;
 	}
 	
 	private void Move (MazeDirection direction) {
@@ -21,27 +24,27 @@ public class Player : MonoBehaviour {
 	}
 	
 	private void FixedUpdate () {
+		fridge = GameObject.FindGameObjectWithTag ("Fridge");
+		rb = fridge.GetComponent<Rigidbody> ();
 		if (Input.GetKey(KeyCode.W)) {
-			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+			//transform.Translate (Vector3.forward * speed * Time.deltaTime);
 			//Move(MazeDirection.North);
+			rb.AddForce(transform.forward * speed);
 		}
 		else if (Input.GetKey(KeyCode.D)) {
-			transform.Translate (Vector3.right * speed * Time.deltaTime);
+			//transform.Translate (Vector3.right * speed * Time.deltaTime);
 			//Move(MazeDirection.East);
+			rb.AddForce(transform.right * speed);
 		}
 		else if (Input.GetKey(KeyCode.S)) {
-			transform.Translate (Vector3.back * speed * Time.deltaTime);
+			//transform.Translate (Vector3.back * speed * Time.deltaTime);
 			//Move(MazeDirection.South);
+			rb.AddForce(transform.forward * speed *-1f);
 		}
 		else if (Input.GetKey(KeyCode.A)) {
-			transform.Translate (Vector3.left * speed * Time.deltaTime);
+			//transform.Translate (Vector3.left * speed * Time.deltaTime);
 			//Move(MazeDirection.West);
-		}
-	}
-
-	void OnCollisionEnter (Collision other){
-		if(other.gameObject.tag == "Wall"){
-			transform.position = new Vector3(transform.position.x - offset,transform.position.y, transform.position.z - offset);
+			rb.AddForce(transform.right * -1f * speed);
 		}
 	}
 }
